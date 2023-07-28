b650be3dfffbf18f3f267552371527203ec09962
SkyRaks <nano.volosin2016@gmail.com>
2023-07-28 12:51:11 -0400

    heroku final

--- src/main/java/com/oliver/BookStore/BookStoreApplication.java
+++ src/main/java/com/oliver/BookStore/BookStoreApplication.java
@@ -3,11 +3,17 @@ package com.oliver.BookStore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class BookStoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BookStoreApplication.class, args);
+//		SpringApplication.run(BookStoreApplication.class, args);
+		int port = Integer.parseInt(System.getenv("PORT"));
+		SpringApplication app = new SpringApplication(BookStoreApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("server.port", String.valueOf(port)));
+		app.run(args);
 	}
 
 }
